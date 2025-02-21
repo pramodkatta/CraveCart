4b91d26d0684460ff78605e4490588c16d5d3ce5
pramodkatta <pramodkatta1@gmail.com>
2025-02-21 14:10:12 +0530

    Login Page

---
+++ src/LoginSignupScreen/LoginScreen.js
@@ -0,0 +1,95 @@
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+
+const LoginScreen = () => {
+  return (
+    <View style = {styles.container}>
+        <StatusBar backgroundColor='white'/>
+        <View style={{paddingVertical: 12, width: '90%', alignSelf: 'center', marginBottom: 10}}>
+        <Text style={{alignSelf: 'center', fontSize: 25, fontWeight: 700}}>Login</Text>
+        </View>
+        
+        <TextInput
+            placeholder='Email'
+            keyboardType='email-address'
+            style={styles.input}
+        />
+        <TextInput
+            placeholder='Password'
+            style={styles.input}
+        />
+        <TouchableOpacity style={styles.loginbutton} onPress={() => alert('Login Successfull!')}>
+            <Text style={styles.loginbuttontxt}>Login</Text>
+        </TouchableOpacity>
+
+        <View style={{marginTop: 10, width: '90%', alignSelf: 'center', flexDirection: 'row', justifyContent: 'flex-start'}}>
+            <View style={{paddingLeft: 10, paddingTop: 10}}>
+                <Text style={{fontSize: 15, fontStyle:'italic'}}>Don't have an account?</Text>
+            </View>
+            <View style={styles.signupbutton}>
+                <TouchableOpacity>
+                    <Text style={styles.signupbuttontxt}>SignUp</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+        
+    </View>
+  )
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F9F9F9',
+        width: '100%',
+    },
+
+    input: {
+        padding: 10,
+        borderColor: 'grey',
+        borderWidth: 1,
+        borderRadius: 25,
+        marginBottom: 10,
+        width: '90 %',
+        backgroundColor: 'white',
+        alignSelf: 'center'
+    },
+
+    loginbutton: {
+        padding: 10,
+        backgroundColor: '#4CAF50',
+        borderRadius: 25,
+        width: '90 %',
+        alignSelf: 'center',
+        elevation: 2
+    },
+
+    loginbuttontxt: {
+        fontSize: 17,
+        fontWeight: '600',
+        color: 'white',
+        alignSelf: 'center'
+    },
+
+    signupbutton: {
+        backgroundColor: '#4CAF50', 
+        borderRadius: 25, 
+        alignSelf: 'center', 
+        padding: 10, 
+        elevation: 2
+    },
+
+    signupbuttontxt: {
+        fontSize: 15, 
+        fontWeight: '600', 
+        color: 'white', 
+        alignSelf: 'center', 
+        paddingHorizontal: 10 
+    }
+
+})
